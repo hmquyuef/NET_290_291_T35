01ef03eebf2cced686ca2e0cbfc30ba5ef0ac800
hmquyuef <quyhm@uef.edu.vn>
2024-04-23 14:52:23 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NET_290_291_T35.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
